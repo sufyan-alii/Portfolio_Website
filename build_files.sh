4e865732057090e013115e9472ac4c6c64374009
sufyan-alii <sufyan7586432@gmail.com>
2024-02-24 04:49:18 +0500

    chnages

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,13 @@
-# build_files.sh
+#!/bin/bash
+
+# Install Python dependencies
 pip install -r requirements.txt
+
+# Create the dist directory if it doesn't exist
+mkdir -p dist
+
+# Collect static files into the dist directory
+# python3.9 manage.py collectstatic --no-input
+
+# Exit with success status
+exit 0
