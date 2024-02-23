f1fcc5ba682d1fe47259c8865bbf2b30684de28b
sufyan-alii <sufyan7586432@gmail.com>
2024-02-24 02:59:19 +0500

    eight chnages

--- Portfolio_Website/settings.py
+++ Portfolio_Website/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 import os
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 
 # Default primary key field type
