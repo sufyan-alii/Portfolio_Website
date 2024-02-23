f5261e336572bef42fcee9b1e27eb663acca8416
sufyan-alii <sufyan7586432@gmail.com>
2024-02-24 03:17:52 +0500

    eight chnages

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,13 @@
-# build_files.sh
+#!/bin/bash
+
+# Install Python dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Create the staticfiles_build directory if it doesn't exist
+mkdir -p staticfiles_build
+
+# Collect static files
+python3.9 manage.py collectstatic --no-input
+
+# Exit with success status
+exit 0
