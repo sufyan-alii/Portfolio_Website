bf4e7abab62791d482bdc7075df97cca9dca7663
sufyan-alii <sufyan7586432@gmail.com>
2024-02-24 02:23:18 +0500

    second chnages

--- Portfolio_Website/settings.py
+++ Portfolio_Website/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 import os
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
--- Portfolio_Website/urls.py
+++ Portfolio_Website/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', include('my_app.urls')),
     path('admin/', admin.site.urls),
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
