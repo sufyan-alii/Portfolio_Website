1c9f27cd224767a98e6f41e2274e2fbfb66f0314
sufyan-alii <sufyan7586432@gmail.com>
2024-02-24 07:45:03 +0500

    chnages

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@
 pip install -r requirements.txt
 
 # Create the dist directory if it doesn't exist
-mkdir -p dist
+mkdir -p static
 
 # Collect static files into the dist directory
 python3.9 manage.py collectstatic --no-input
