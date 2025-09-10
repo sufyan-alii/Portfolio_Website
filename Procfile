7e6c3b5f45989e0e7026c9d6a5ee5400f6163138
sufyan-alii <sufyan7586432@gmail.com>
2025-09-10 10:31:46 +0500

    new update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn Portfolio_Website.wsgi
---
+++ build_files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Create the dist directory if it doesn't exist
+mkdir -p dist
+mkdir -p static
+
+
+# Collect static files into the dist directory
+python3.9 manage.py collectstatic --no-input
+
+# Exit with success status
+exit 0
